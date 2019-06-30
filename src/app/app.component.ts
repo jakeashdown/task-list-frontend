380fd3cb351b76a991c8cda11a2a80740a83f25c
jake.ashdown@protonmail.com <jake.ashdown@protonmail.com>
2019-06-30 12:30:02 +0200

    Fetch and display task list from REST API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rest-frontend-demo';
+  allTasks: any
+  allTasksUrl = "http://localhost:8080/task"
+
+  constructor(private http: HttpClient) {}
+
+  taskServiceGetAllTasks() {
+    return this.http
+      .get<any[]>(this.allTasksUrl)
+      .pipe(map(data => data));
+  }
+
+  getAllTasks(): void {
+    this.taskServiceGetAllTasks()
+      .subscribe(
+        allTasksFromApi => {
+          this.allTasks = allTasksFromApi;
+          console.log(this.allTasks);
+        }
+      )
+  }
+
+  ngOnInit() {
+    this.getAllTasks();
+  }
 }
